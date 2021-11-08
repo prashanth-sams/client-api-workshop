d35cc87e40264f7cd3414349b34b5e379f020830
prashanth-sams <sams.prashanth@gmail.com>
2021-11-08 18:28:42 +0400

    added travis, hooks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,18 @@ require 'json'
 
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
   end
+
+  config.filter_run_when_matching :focus
+  config.expose_dsl_globally = true
 end
 
 ClientApi.configure do |config|
